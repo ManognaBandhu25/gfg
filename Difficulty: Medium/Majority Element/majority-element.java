1ea1efcf3349fae40d72294133cd36b8e01cca05
Manogna Bandhu <manognabandhu25@gmail.com>
2025-07-31 13:52:20 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Majority Element/majority-element.java
@@ -0,0 +1,20 @@
+class Solution {
+    static int majorityElement(int arr[]) {
+        // code here
+      int candidate = -1,count=0;
+      for(int num : arr){
+          if(count == 0) {
+              candidate = num;
+          }
+          count+=(num == candidate)?1:-1;
+        }
+        count=0;
+        for(int num:arr){
+            if(num==candidate){
+            count++;
+            }
+        }
+        return (count>arr.length/2)?candidate : -1;
+         
+    }
+}
