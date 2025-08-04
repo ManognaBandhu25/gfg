c37ba4f7dd2638a30ab3bc824b4f6813556905bf
Manogna Bandhu <manognabandhu25@gmail.com>
2025-08-04 09:44:52 +0530

    Updated Solution - GfG to GitHub

---
+++ Difficulty: Hard/Maximum sum Rectangle/maximum-sum-rectangle.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int maxRectSum(int mat[][]) {
+        int n = mat.length;
+        int m = mat[0].length;
+        int maxSum = Integer.MIN_VALUE;
+
+        for (int left = 0; left < m; left++) {
+            int[] temp = new int[n];
+
+            for (int right = left; right < m; right++) {
+                for (int i = 0; i < n; i++) {
+                    temp[i] += mat[i][right];
+                }
+                int currentMax = kadane(temp);
+
+                maxSum = Math.max(maxSum, currentMax);
+            }
+        }
+
+        return maxSum;
+    }
+
+
+    private int kadane(int[] arr) {
+        int maxEndingHere = arr[0];
+        int maxSoFar = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            maxEndingHere = Math.max(arr[i], maxEndingHere + arr[i]);
+            maxSoFar = Math.max(maxSoFar, maxEndingHere);
+        }
+
+        return maxSoFar;
+    }
+}
