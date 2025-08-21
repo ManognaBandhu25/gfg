7dc497a234eb53c21f3235a6919f54bb4f0f1684
Manogna Bandhu <manognabandhu25@gmail.com>
2025-08-22 00:29:33 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Median in a row-wise sorted Matrix/median-in-a-rowwise-sorted-matrix.java
@@ -0,0 +1,20 @@
+class Solution {
+    int median(int mat[][]) {
+        // code here
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        
+        for(int i=0; i<mat.length; i++){
+            for(int j=0; j<mat[0].length; j++){
+                pq.add(mat[i][j]);
+            }
+        }
+        
+        int length = pq.size();
+        
+        for(int i=0; i<length/2; i++){
+            pq.remove();
+        }
+        
+        return pq.peek();
+    }
+}
