4b3f38effdaae6d4704fa1ac795cc371f2d2d014
Manogna Bandhu <manognabandhu25@gmail.com>
2025-09-19 11:02:30 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Min Add to Make Parentheses Valid/min-add-to-make-parentheses-valid.java
+++ Difficulty: Medium/Min Add to Make Parentheses Valid/min-add-to-make-parentheses-valid.java
@@ -1,37 +1,20 @@
 class Solution {
     public int minParentheses(String s) {
         // code here
-    
-    StringBuilder sb = new StringBuilder(s);
-    Stack<Integer> openBracketPos = new Stack<>();
-    for (int i = 0; i < s.length(); i++) {
-        if (sb.charAt(i) == '(') {
-            openBracketPos.add(i);
-        } else if (sb.charAt(i) == ')') {
-            if (openBracketPos.isEmpty()) {
-                sb.setCharAt(i, '*');
-            } else {
-                openBracketPos.pop();
+        int n=s.length();
+        int ans=0;
+        int open=0;
+        for(int i=0;i<n;i++)
+        {
+            char c=s.charAt(i);
+            if(c=='(')open++;
+            else 
+            {
+                if(open==0)ans++;
+                else open--;
             }
         }
-    }
-
-    while (!openBracketPos.isEmpty()) {
-        sb.setCharAt(openBracketPos.pop(), '*');
-    }
-
-    StringBuilder result = new StringBuilder();
-    for (int i = 0; i < sb.length(); i++) {
-        if (sb.charAt(i) != '*') {
-            result.append(sb.charAt(i));
-        }
-    }
-
-    return (int) sb.chars().filter(ch -> ch == '*').count();
-
-
-
+       return ans+open;
+        
     }
 }
-
-
