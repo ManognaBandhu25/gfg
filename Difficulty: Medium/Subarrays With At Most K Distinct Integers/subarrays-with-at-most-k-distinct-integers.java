ff0433087a09d77a4639c00fef5c304ff50449a7
Manogna Bandhu <manognabandhu25@gmail.com>
2026-01-09 10:43:58 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Subarrays With At Most K Distinct Integers/subarrays-with-at-most-k-distinct-integers.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int countAtMostK(int arr[], int k) {
+        // code here
+        int n=arr.length;
+        long total=n*(n+1)/2;
+        
+        int i=0;
+        int j=0;
+        int ans=0;
+        HashMap<Integer,Integer> map=new HashMap<>();
+        while(j<n)
+        {
+            map.put(arr[j],map.getOrDefault(arr[j],0)+1);
+           // System.out.println(map);
+            while(map.size()>k)
+            {
+                ans+=n-j;
+                
+                  map.put(arr[i],map.get(arr[i])-1);
+                  
+                  if(map.get(arr[i])==0) 
+                  map.remove(arr[i]);
+                
+                i++;
+            }
+            j++;
+        }
+        
+        return (int)total-ans;
+    }
+}
