9d665f4666ce72f33228647d2ed236b351ea7565
Manogna Bandhu <manognabandhu25@gmail.com>
2026-01-10 19:04:36 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Substrings with K Distinct/substrings-with-k-distinct.java
+++ Difficulty: Medium/Substrings with K Distinct/substrings-with-k-distinct.java
@@ -1,40 +1,32 @@
-// User function Template for Java
-
-import java.util.*;
-
 class Solution {
-    int countSubstr(String s, int k) {
-        return countAtMostK(s, k) - countAtMostK(s, k - 1);
-    }
-
-    private int countAtMostK(String s, int k) {
-        int left = 0, right = 0;
-        int count = 0;
-        Map<Character, Integer> freqMap = new HashMap<>();
-
-        for (right = 0; right < s.length(); right++) {
-            char c = s.charAt(right);
-            freqMap.put(c, freqMap.getOrDefault(c, 0) + 1);
-
-            while (freqMap.size() > k) {
-                char leftChar = s.charAt(left);
-                freqMap.put(leftChar, freqMap.get(leftChar) - 1);
-                if (freqMap.get(leftChar) == 0) {
-                    freqMap.remove(leftChar);
-                }
-                left++;
-            }
-
-            count += (right - left + 1);
+    private int atMostK(String s, int k){
+        char[] str = s.toCharArray();
+         int i =0; 
+         int j =0;
+        int[] hash = new int[26];
+        int distinctCount=0;
+        int ans = 0;
+        
+        while(j<str.length){
+            
+            hash[str[j]-'a']++;
+            
+            if(hash[str[j] - 'a'] == 1)distinctCount++; // seeing for the first time
+            
+           while(distinctCount > k){
+               hash[str[i] - 'a']--;
+               if(hash[str[i] - 'a'] == 0){
+                 distinctCount--;
+               }
+               i++;
+           }
+           ans+=(j-i+1);
+           j++;
         }
-
-        return count;
+        return ans;
+        
     }
-
-    public static void main(String[] args) {
-        Solution sol = new Solution();
-        System.out.println(sol.countSubstr("abc", 2)); // Output: 2
-        System.out.println(sol.countSubstr("aba", 2)); // Output: 3
-        System.out.println(sol.countSubstr("aa", 1));  // Output: 3
+    public int countSubstr(String s, int k) {
+          return atMostK(s,k) -atMostK(s,k-1);
     }
 }
